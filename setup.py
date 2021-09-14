1c019f66864a8a5491279b43d210fd2e36bd5876
Oliver Fabel <oliver.fabel@fhnw.ch>
2021-09-14 19:32:38 +0200

    add missing packages

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-
+from setuptools import find_packages
 from setuptools import setup
 
 setup(name='pytm',
       version='0.1.0',
       author='Oliver Fabel',
       author_email='oliver.fabel@fhnw.ch',
-      packages=['pytm'],
+      packages=find_packages(include=['pytm', 'pytm.*']),
       description='Skeleton for a Python Tool Manager exercise or tool',
       install_requires=[
           'Flask==2.0.1',
