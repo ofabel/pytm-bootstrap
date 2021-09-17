f352e8db9826f39c78ced9f9df0fe2f014727005
Oliver Fabel <oliver.fabel@fhnw.ch>
2021-09-17 04:39:30 +0200

    get application secret from environment variable

--- pytm/abstract_exercise.py
+++ pytm/abstract_exercise.py
@@ -1,4 +1,5 @@
 import abc
+from os import getenv
 
 from .context import Context
 from .create_app import create_app
@@ -6,8 +7,9 @@ from .output import OutputBuilder as Output
 
 
 class AbstractExercise(abc.ABC):
-    def __new__(cls, unique_id: str, secret: str, static_folder_path: str = None, *args, **kwargs):
+    def __new__(cls, unique_id: str, static_folder_path: str = None, *args, **kwargs):
         exercise: AbstractExercise = super().__new__(cls, *args, **kwargs)
+        secret: str = getenv('PYTM_SECRET', 'b9ceb55b-4ee4-4af4-b7b0-a01c4814f4c7')
         context: Context = Context(unique_id, secret, exercise)
         exercise._context = context
 
