e5e1653c76a4999c3640d13154e35823a05458bd
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2023-06-20 14:51:01 +0200

    get exercise id from env variable

--- src/pytmlib/abstract_exercise.py
+++ src/pytmlib/abstract_exercise.py
@@ -11,18 +11,26 @@ from .decorators import ENTRYPOINT_MARKER
 from .output import OutputBuilder as Output
 from .types import Action
 
+PYTM_UID_ENV = 'PYTM_UID'
+PYTM_SECRET_ENV = 'PYTM_SECRET'
+HOSTNAME_ENV = 'HOSTNAME'
+
 
 class AbstractExercise(abc.ABC):
-    def __new__(cls, unique_id: str, static_folder_path: str = None, *args, **kwargs):
+    def __new__(cls, unique_id: str = None, static_folder_path: str = None, *args, **kwargs):
         exercise: AbstractExercise = super().__new__(cls, *args, **kwargs)
 
+        unique_id = os.getenv(PYTM_UID_ENV, unique_id)
         secret, fallback = AbstractExercise._get_secret()
         context: Context = Context(unique_id, secret, exercise)
 
         exercise._context = context
 
+        if unique_id is None:
+            logging.warning('missing %s environment variable', PYTM_UID_ENV)
+
         if fallback:
-            logging.warning('missing PYTM_SECRET environment variable')
+            logging.warning('missing %s environment variable', PYTM_SECRET_ENV)
 
         return create_app(context, static_folder_path)
 
@@ -43,9 +51,9 @@ class AbstractExercise(abc.ABC):
 
     @staticmethod
     def _get_secret() -> Tuple[str, bool]:
-        hostname: str = os.getenv('HOSTNAME', os.name)
+        hostname: str = os.getenv(HOSTNAME_ENV, os.name)
         fallback: uuid = uuid.uuid5(uuid.NAMESPACE_DNS, hostname)
-        secret: str = os.getenv('PYTM_SECRET')
+        secret: str = os.getenv(PYTM_SECRET_ENV)
         use_fallback: bool = secret is None
 
         return str(fallback) if use_fallback else secret, use_fallback
